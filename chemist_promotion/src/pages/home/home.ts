6277a9d4b1f9f0be721bd26b5bc1081ce7ad1990
wuangyalin <wuangyalin@gmail.com>
2017-02-10 11:45:51 +1100

    Updated
    Updated Loading Spinner

--- chemist_promotion/src/pages/home/home.ts
+++ chemist_promotion/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController,LoadingController} from 'ionic-angular';
 import { DataService } from '../../providers/data-service';
 import { Storage } from '@ionic/storage';
 
@@ -10,7 +10,10 @@ import { Storage } from '@ionic/storage';
 export class HomePage {
   rows: any;
   categories: any;
-  constructor(public navCtrl: NavController, private data: DataService, public storage: Storage) {
+  loader: any;
+  constructor(public loadingCtrl: LoadingController,public alertCtrl: AlertController, public navCtrl: NavController, private data: DataService, public storage: Storage) {
+      this.presentLoading();
+
   }
 
   ionViewDidLoad(){
@@ -19,18 +22,44 @@ export class HomePage {
         this.categories = value;
         this.rows = Array.from(Array(Math.ceil(this.categories.length / 2)).keys());
         console.log(value);
+            this.dismissLoading();
+
       }else{
         console.log('no data');
         this.data.loadData().then(data => {
           this.categories = data;
           this.rows = Array.from(Array(Math.ceil(this.categories.length / 2)).keys());
           this.storage.set('promotion_products',data);
+          this.dismissLoading();
         });
       }
     })
   }
-  clearStorage(){
-    this.storage.remove('promotion_products');
-  }
 
+  clearStorage(){
+    this.storage.remove('promotion_products').then(success =>{
+      let title = '';
+      let content = 'Local Storage cleared successfully';
+      this.showAlert(title,content);
+    });
+
+  }
+  showAlert(title,content){
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: content,
+      buttons:['OK']
+    });
+    alert.present();
+  }
+  dismissLoading(){
+    this.loader.dismiss();
+  }
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Web crawling...",
+      dismissOnPageChange: true
+    });
+    this.loader.present();
+  }
 }
